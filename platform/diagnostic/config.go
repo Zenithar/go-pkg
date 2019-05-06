c4282135ed99a67036f532f9564e54df089713e9
Thibault NORMAND <me@zenithar.org>
2019-05-07 00:20:29 +0200

    feat(platform): Disable gops by default.

--- platform/diagnostic/config.go
+++ platform/diagnostic/config.go
@@ -3,7 +3,7 @@ package diagnostic
 // Config holds information for diagnostic handlers.
 type Config struct {
 	GOPS struct {
-		Enabled   bool   `toml:"enabled" default:"true" comment:"Enable GOPS agent"`
+		Enabled   bool   `toml:"enabled" default:"false" comment:"Enable GOPS agent"`
 		RemoteURL string `toml:"remoteDebugURL" comment:"start a gops agent on specified URL. Ex: localhost:9999"`
 	}
 	PProf struct {
